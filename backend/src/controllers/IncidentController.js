bfdf3a86475f8a7df7371a7df9d4ec3b9ff945a8
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2020-03-29 11:52:57 -0300

    fix incident id on create

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -28,13 +28,14 @@ async function create(req, res) {
   const { title, description, value } = req.body;
   const ong_id = req.headers.authorization;
 
-  const [id] = await connection('incidents').insert({
-    title,
-    description,
-    value,
-    ong_id
-  });
-  console.log(id);
+  const [id] = await connection('incidents')
+    .returning('id')
+    .insert({
+      title,
+      description,
+      value,
+      ong_id
+    });
 
   return res.json({ id });
 }
